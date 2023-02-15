5c1bd91626c99e86e2e916726787d27162c00b29
GUNiclasEricsson <125461245+GUNiclasEricsson@users.noreply.github.com>
2023-02-15 12:48:02 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,25 @@
+LEVEL_MAP = [
+"XXXXXXXXXXXXXXXXXXXX",
+"X             X    X",
+"X     XX           X",
+"X         X        X",
+"X    P       XXXX  X",
+"X XXXXXX           X",
+"X                  X",
+"X         XXX      X",
+"X  XX  X  XXXX     X",
+"X  XX     XXXX     X",
+"XXXXXXXXXXXXXXXXXXXX"]
+
+
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+TILE_SIZE = 64
+
+BG_COLOR = (0,0,0)
+PLAYER_COLOR = (230,230,0)
+TILE_COLOR = (20,200,20)
+
+
+FPS = 60
