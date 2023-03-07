f107c97dbcee15c1d8cf655b60cfd448aff95566
Avve2000 <125461412+Avve2000@users.noreply.github.com>
2023-03-07 15:59:48 +0100

    Add files via upload

--- pygameintro2.py
+++ pygameintro2.py
@@ -9,28 +9,45 @@ pygame.display.set_caption('First')
 clock = pygame.time.Clock()
 
 def display_score():
-    current_time = pygame.time.get_ticks()
-    score_surf = test_font.render(current_time, False, (64,64,64))
+
+    current_time = int(pygame.time.get_ticks()/1000) - start_time
+    score_surf = name_font.render(f'Score: {current_time}', False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400,50))
     screen.blit(score_surf,score_rect)
-    print(current_time)
+    return current_time
 
-test_font = pygame.font.Font('fonts/RETRO_SPACE.ttf', 50)
+score = 0
+
+name_font = pygame.font.Font('fonts/RETRO_SPACE.ttf', 50)
 
 sky_surf = pygame.image.load('images/sky.png').convert()
 ground_surf = pygame.image.load('images/Ground.png').convert()
 
-#score_surf = test_font.render('First', False, (64, 64, 64))
-#score_rect = score_surf.get_rect(center = (400, 100))
+name_font = pygame.font.Font('fonts/RETRO_SPACE.ttf', 50)
+name_surf = name_font.render('First', False, (64, 64, 64))
+name_rect = name_surf.get_rect(center = (400, 100))
+
+instruct_font = pygame.font.Font('fonts/RETRO_SPACE.ttf', 30)
+instruct_surf = instruct_font.render('Press ESC to start the game', False, (138, 25, 64))
+instruct_rect = instruct_surf.get_rect(center = (400, 300))
 
 snail_surf = pygame.image.load('images/snail1.gif').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (600, 300))
 
-player_surf = pygame.image.load('images/spr_greaser.png').convert_alpha()
+player_surf = pygame.image.load('images/player/spr_greaser.png').convert_alpha()
 player_rect = player_surf.get_rect(topleft = (80, 270))
+
+player_ko = pygame.image.load('images/player/spr_greaser_ko.png').convert_alpha()
+player_ko = pygame.transform.rotozoom(player_ko, 0,2)
+player_ko_rect = player_ko.get_rect(center = (400, 200))
+
 player_gravity = 0
 
-game_active = True
+start_time = 0
+
+
+
+game_active = False
 
 
 
@@ -52,10 +69,8 @@ while True:
 
         screen.blit(sky_surf,(0,0))
         screen.blit(ground_surf,(0,300))
-        #pygame.draw.rect(screen, "#c0e8ec", score_rect)
-        #pygame.draw.rect(screen, "#c0e8ec", score_rect,10)
-        #screen.blit(score_surf, score_rect)
-        display_score()
+
+        score = display_score()
 
 
 
@@ -83,13 +98,28 @@ while True:
 
 
     else:
-        screen.fill("Yellow")
+        screen.fill((94, 129, 162))
+
+        pygame.draw.rect(screen, "#c0e8ec", name_rect)
+        pygame.draw.rect(screen, "#c0e8ec", name_rect,10)
+        screen.blit(name_surf, name_rect)
+
+        pygame.draw.rect(screen, "#c0e8ec", instruct_rect)
+        pygame.draw.rect(screen, "#c0e8ec", instruct_rect,10)
+        screen.blit(instruct_surf, instruct_rect)
+
+        score_message = name_font.render(f'Your score: {score}', False, (64, 64, 64))
+        #blitta den
+
+        screen.blit(player_ko, player_ko_rect)
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 game_active = True
                 snail_rect.left = 800
                 player_rect.left = 80
+                start_time = int(pygame.time.get_ticks()/1000)
+
 
 
 
